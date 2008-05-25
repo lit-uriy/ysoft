003dcbfc0f17c853b20073c394ccfa90f9fc1b0f
lit-uriy <lit-uriy@d7ba3245-3e7b-42d0-9cd4-356c8b94b330>
2008-05-25 21:27:56 +0000

    * Сделал так, чтобы тикеты составлялись для 3-х файлов: linguist_ru.ts assistant_ru.ts qt_ru.ts, Но надо, еще сделать, чтобы время тикетов отличалось!

--- rtt-tool-2/trunk/main.cpp
+++ rtt-tool-2/trunk/main.cpp
@@ -6,7 +6,8 @@
 #include <QDir>
 #include <QDebug>
 #include <QStringList>
-
+#include <QList>
+#include <QPair>
 
 
 #include "tsreader.h"
@@ -28,42 +29,72 @@ int main(int argc, char *argv[])
   QString	milestone;
   QString	dbname;
   bool		ok;
-  QStringList list;
+  
+  typedef QPair<QString, QString> SPair;
+  
+  QStringList	list;
+  //SPair			pair;
+  QList<SPair>	listpair;
+  
   QCoreApplication app(argc, argv);
   
 	// Устанавливаем кодеки
 	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
 	
-	dbname = "trac.db";
-	tsname = "linguist_ru.ts"; // "assistant_ru.ts" "qt_ru.ts"  
-	component = tr("ts Qt Linguist"); // tr("ts Qt Assistant"); tr("ts Qt") 
-	milestone = tr("Перевод TS-файлов");
-	
 	tsdir = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-	tsfile.setFileName(tsdir.absolutePath() + "/" + tsname);
-	qDebug() << "TS File: " << tsfile.fileName() << "\n\r";
+	dbname = "trac.db";
+	milestone = tr("Перевод TS-файлов");
+
+	// Свяжем названия файлов с компонентами
+	/*
+	pair.first("linguist_ru.ts"); pair.second(tr("ts Qt Linguist"));
+	listpair.append(pair);
+	pair.insert("assistant_ru.ts"); pair.second(tr("ts Qt Assistant"));
+	listpair.append(pair);
+	pair.insert("qt_ru.ts"); pair.second(tr("ts Qt"));
+	listpair.append(pair);
+	*/
+
+	listpair.append(SPair("linguist_ru.ts",tr("ts Qt Linguist")));
+	listpair.append(SPair("assistant_ru.ts",tr("ts Qt Assistant")));
+	listpair.append(SPair("qt_ru.ts",tr("ts Qt")));
 	
-    tsfile.open( QIODevice::ReadOnly);
-	// Читаем список секций из ts-файла
-	qDebug() << "---------- Read sections from *.ts ----------" << "\n\r";
-    ok = GetListFrom(&tsfile, list);
-	tsfile.close();
-	if (!ok)
-		return 1;
-
-	// Открываем БД
-	qDebug() << "---------- Open Db ----------" << "\n\r";
-	ok = OpenDb(dbname);
-	if (!ok)
-		return 1;
-
-	// Создаем тикеты
-	qDebug() << "---------- Create tickets ----------" << "\n\r";
-	ok = CreateTickets(list, component, milestone, tsname);
-	if (!ok)
-		return 1;	
+	/*
+	tsname = "linguist_ru.ts"; // "assistant_ru.ts" "qt_ru.ts"  
+	component = tr("ts Qt Linguist"); // tr("ts Qt Assistant"); tr("ts Qt")
+	*/
+	foreach (SPair pair, listpair)
+	{
+		tsname = pair.first;
+		component = pair.second;
+		
+		// 
+		tsfile.setFileName(tsdir.absolutePath() + "/" + tsname);
+		qDebug() << "TS File: " << tsfile.fileName() << "\n\r";
+		qDebug() << "Component: " << component << "\n\r";
+		
+		tsfile.open( QIODevice::ReadOnly);
+		// Читаем список секций из ts-файла
+		qDebug() << "---------- Read sections from *.ts ----------" << "\n\r";
+		ok = GetListFrom(&tsfile, list);
+		tsfile.close();
+		if (!ok)
+			return 1;
+		
+		// Открываем БД
+		qDebug() << "---------- Open Db ----------" << "\n\r";
+		ok = OpenDb(dbname);
+		if (!ok)
+			return 1;
+		
+		// Создаем тикеты
+		qDebug() << "---------- Create tickets ----------" << "\n\r";
+		ok = CreateTickets(list, component, milestone, tsname);
+		if (!ok)
+			return 1;	
+	}
 	
 	qDebug() << "---------- End of Programm ----------" << "\n\r";
-	//return 0;
-	return app.exec();
+	return 0;
+	//return app.exec();
 }
