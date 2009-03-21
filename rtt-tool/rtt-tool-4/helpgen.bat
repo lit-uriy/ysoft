5741074785c8601ef2a78cccf8373e39f92f60d3
lit-uriy <lit-uriy@d7ba3245-3e7b-42d0-9cd4-356c8b94b330>
2009-03-21 01:46:47 +0000

    * rtt-tool-4: * Добавил файлы проекта справки Qt Assistent.

---
+++ rtt-tool/rtt-tool-4/helpgen.bat
@@ -0,0 +1,5 @@
+qhelpgenerator.exe assistant_ru.qhp -o ../qch/assistant_ru.qch
+qhelpgenerator.exe designer_ru.qhp -o ../qch/designer_ru.qch
+qhelpgenerator.exe linguist_ru.qhp -o ../qch/linguist_ru.qch
+qhelpgenerator.exe qmake_ru.qhp -o ../qch/qmake_ru.qch
+qhelpgenerator.exe qt_ru.qhp -o ../qch/qt_ru.qch
