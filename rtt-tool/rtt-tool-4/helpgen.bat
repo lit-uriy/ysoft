b2ee8b22d6817da2ae9cf79553828477d6dc6a51
lit-uriy <lit-uriy@d7ba3245-3e7b-42d0-9cd4-356c8b94b330>
2009-03-22 16:00:36 +0000

    * rtt-tool-4: * Обновил файлы проекта справки Qt Assistent (4.4.3).

--- rtt-tool/rtt-tool-4/helpgen.bat
+++ rtt-tool/rtt-tool-4/helpgen.bat
@@ -1,5 +1,7 @@
-qhelpgenerator.exe assistant_ru.qhp -o ../qch/assistant_ru.qch
-qhelpgenerator.exe designer_ru.qhp -o ../qch/designer_ru.qch
-qhelpgenerator.exe linguist_ru.qhp -o ../qch/linguist_ru.qch
-qhelpgenerator.exe qmake_ru.qhp -o ../qch/qmake_ru.qch
-qhelpgenerator.exe qt_ru.qhp -o ../qch/qt_ru.qch
+cd html
+qhelpgenerator.exe assistant_ru.qhp -o ../assistant_ru.qch
+qhelpgenerator.exe designer_ru.qhp -o ../designer_ru.qch
+qhelpgenerator.exe linguist_ru.qhp -o ../linguist_ru.qch
+qhelpgenerator.exe qmake_ru.qhp -o ../qmake_ru.qch
+qhelpgenerator.exe qt_ru.qhp -o ../qt_ru.qch
+pause
