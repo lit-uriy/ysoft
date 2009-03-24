439f4c24b1af87b164da734703637e44b8ffd43b
lit-uriy <lit-uriy@d7ba3245-3e7b-42d0-9cd4-356c8b94b330>
2009-03-24 21:58:42 +0000

    * rtt-tool-4: * Подправил bat'ник.

--- rtt-tool/rtt-tool-4/helpgen.bat
+++ rtt-tool/rtt-tool-4/helpgen.bat
@@ -1,7 +1,15 @@
-cd html
+cd html-assistant
 qhelpgenerator.exe assistant_ru.qhp -o ../assistant_ru.qch
+cd ../
+cd html-designer
 qhelpgenerator.exe designer_ru.qhp -o ../designer_ru.qch
+cd ../
+cd html-linguist
 qhelpgenerator.exe linguist_ru.qhp -o ../linguist_ru.qch
+cd ../
+cd html-qmake
 qhelpgenerator.exe qmake_ru.qhp -o ../qmake_ru.qch
+cd ../
+cd html-qt
 qhelpgenerator.exe qt_ru.qhp -o ../qt_ru.qch
 pause
