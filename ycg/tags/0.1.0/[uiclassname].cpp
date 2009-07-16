850fbe8f53f4239c1c9585f490875535b2b6fcd3
lit-uriy <lit-uriy@d7ba3245-3e7b-42d0-9cd4-356c8b94b330>
2009-07-16 00:20:49 +0000

    YCG-0.1.0: Сделал метку задним числом.

---
+++ ycg/tags/0.1.0/[uiclassname].cpp
@@ -0,0 +1,17 @@
+/*!
+ *	\file	[classname].cpp
+ *	\brief	Реализация класса "[ClassName]".
+ */
+
+#include <QtGui>
+
+#include "[classname].h"
+
+
+[ClassName]::[ClassName](QWidget* p_parent) : QWidget(p_parent)
+{
+	setupUi(this);
+
+    setWindowTitle(tr("Тестовое окно"));
+}
+
